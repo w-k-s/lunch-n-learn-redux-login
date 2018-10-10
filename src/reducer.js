8da8cad14fd04b57b2c8b3377f2717d21bdd221c
Waqqas Sheikh <wsheikh@tribalscale.com>
2018-10-10 15:33:38 +0400

    create_actions

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,21 @@
+const UPDATE_FORM = 'UPDATE_FORM';
+const SUBMIT_FORM = 'SUBMIT_FORM';
+
+export const updateForm = (key, value) => {
+	return {
+		type: UPDATE_FORM,
+		data: {
+			[key]: value
+		}
+	}
+}
+
+export const submitForm = () => {
+	return {
+		type: SUBMIT_FORM
+	}
+}
+
 const INITIAL_STATE = {
 	form:{},
 	responseData:null,
@@ -7,7 +25,18 @@ const INITIAL_STATE = {
 
 export const reducer =  (state = INITIAL_STATE, action)=>{
 	switch(action.type){
+		case UPDATE_FORM:
+			return {
+				...state,
+				form:{
+					...state.form,						
+					...action.data,
+				}
+			}
+		case SUBMIT_FORM:
+			//TODO: Handle using redux-saga
+			console.log(state.form);
 		default:
-			return INITIAL_STATE;
+			return state;
 	}
 }
